01b67c87087457ec4f21dd8c4ca3cb76bd458220
Lukasz Kondracki <technikum@kondracki.info>
2022-10-11 09:23:16 +0200

    Theme switching

--- theme.js
+++ theme.js
@@ -1,7 +1,27 @@
 const btn = document.getElementById('theme');
+const html = document.getElementsByTagName('html')[0];
+
+if (window.localStorage.getItem('theme') === 'light') {
+    html.classList.add('light');
+} else {
+    html.classList.remove('light');
+}
 
 btn.addEventListener('click', () => {
 
-    
+    if (html.classList.contains('light')) {
+        html.classList.remove('light');
+        btn.innerText = '🌞';
+        window.localStorage.setItem('theme', 'dark');
+    } else {
+        html.classList.add('light');
+        btn.innerText = '🌚';
+        window.localStorage.setItem('theme', 'light');
+    }
 
 });
+
+// Cookies       ->  wysyłane z żądaniami, można ustawiać z serwera
+// LocalStorage  ->  tylko po stronie klienta
+// IndexedDB     ->  prosta baza danych w przeglądarce
+// WebSQL        ->  relacyjna baza danych w przeglądarce
